2b0826fe2e076c7dc79df42920358b4b21a0f7ac
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-03 08:53:34 +0300

    rest Hello World

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,11 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "First post",
+        content:
+          "This is my first post using rest api, basically its Rest Api's hello world",
+      },
+    ],
+  });
+};
