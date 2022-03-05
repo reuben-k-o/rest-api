6e14f2f996b962af9f23f8a46d6a9555f64ac76c
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-05 07:00:04 +0300

    parsing json

--- controllers/feed.js
+++ controllers/feed.js
@@ -9,3 +9,13 @@ exports.getPosts = (req, res, next) => {
     ],
   });
 };
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.json({
+    message: "Post created successfully",
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
