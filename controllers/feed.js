60b5eef280be4b49715e6055049d52dadf9df4db
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-06 10:12:17 +0300

    serve post request

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,9 +2,15 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First post",
         content:
           "This is my first post using rest api, basically its Rest Api's hello world",
+        imageUrl: "images/watch.png",
+        creator: {
+          name: "Reuben",
+        },
+        createdAt: new Date(),
       },
     ],
   });
@@ -16,6 +22,14 @@ exports.createPost = (req, res, next) => {
 
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: {
+        name: "Reuben",
+      },
+      createdAt: new Date(),
+    },
   });
 };
