57a05a4d10070a74759daf38f6468d77c954b676
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-07 23:37:10 +0300

    server-side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,6 +19,15 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, you entered invalid data!",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const feedController = require("../controllers/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    check("title").trim().isLength({ min: 7 }),
+    check("content").trim().isLength({ min: 5 }),
+  ],
+
+  feedController.createPost
+);
 
 module.exports = router;
