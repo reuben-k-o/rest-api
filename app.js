e1c5f17fdf41a24a9e5abd735847ef5302dfb247
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-09 21:48:19 +0300

    setting up post model

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
+const config = require("./config");
 
 const app = express();
 
@@ -19,4 +21,9 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
-app.listen(8080);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
