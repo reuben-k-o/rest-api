ba599d20a43e4ba1c45ba8fa8b8cc69caa8379e6
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-24 07:50:43 +0300

    cleaned up setHeader

--- app.js
+++ app.js
@@ -40,7 +40,10 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST,DELETE,PATCH, PUT");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, DELETE, PATCH, PUT"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
